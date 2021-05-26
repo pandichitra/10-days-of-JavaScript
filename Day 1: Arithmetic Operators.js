e65b8374afe504d09a2ef2cfc57cc4122923b0f3
Pandi Chitra R <42309745+pandichitra@users.noreply.github.com>
2021-05-26 10:08:27 +0530

    Create Day 1: Arithmetic Operators.js

---
+++ Day 1: Arithmetic Operators.js
@@ -0,0 +1,62 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    area=length*width;// Write your code here
+    
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter = 2 * (length + width);// Write your code here
+    
+    return perimeter;
+}
+_________________________________________________________________________________________________________________________________________________________________________________
+TASK IS:
+Task
+
+Complete the following functions in the editor below:
+
+getArea(length, width): Calculate and return the area of a rectangle having sides length and width .
+getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides length  and width .
+
